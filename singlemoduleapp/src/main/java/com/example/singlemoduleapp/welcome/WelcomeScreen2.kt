2eb2b450bafcd5000457c6f3f82f17d387ae1ef7
Sascha Mäkler <maekler@adesso-mobile.de>
2023-10-06 07:43:20 +0200

    removed code for exercise 2

---
+++ singlemoduleapp/src/main/java/com/example/singlemoduleapp/welcome/WelcomeScreen2.kt
@@ -0,0 +1,39 @@
+package com.example.singlemoduleapp.welcome
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.singlemoduleapp.NavGraphs
+import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+
+@WelcomeNavGraph
+@Destination
+@Composable
+fun WelcomeScreen2(navigator: DestinationsNavigator) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        Text(text = "WelcomeScreen 2")
+
+        Spacer(modifier = Modifier.height(30.dp))
+
+        Button(onClick = {
+            navigator.navigate(NavGraphs.home) // or directly HomeScreenDestination
+        }) {
+            Text(text = "Take me Home (Country Roads...)")
+        }
+    }
+}
+
